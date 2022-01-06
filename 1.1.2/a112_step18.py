39c26c8b63a42c4dd01dd19c5d1aed4a67415935
James Mills <STEMfromMills@users.noreply.github.com>
2022-01-06 13:41:55 -0600

    added file

---
+++ 1.1.2/a112_step18.py
@@ -0,0 +1,20 @@
+# import turtle module
+import turtle as trtl
+
+# create turtle object
+painter = trtl.Turtle()
+
+# set the pen and fill colors
+# then draw a circle
+
+
+# move the turtle to another part of the screen
+
+
+# change both the pen and fill colors
+# then draw a polygon of your choice
+
+
+# create screen object and make it persist
+wn = trtl.Screen()
+wn.mainloop()
