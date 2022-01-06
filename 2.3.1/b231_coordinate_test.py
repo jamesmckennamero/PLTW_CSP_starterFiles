78d8c4e092a91ba88963b5c6cfc0c0a9357d7e21
James Mills <STEMfromMills@users.noreply.github.com>
2022-01-06 15:18:14 -0600

    Add files via upload

---
+++ 2.3.1/b231_coordinate_test.py
@@ -0,0 +1,12 @@
+#   b231_coordiante_test
+import turtle as trtl
+
+screen = trtl.getscreen()
+drawer = trtl.Turtle()
+
+drawer.shape("square")
+drawer.color("blue")
+
+drawer.goto(-467,392)
+
+screen.mainloop()
