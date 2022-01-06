14dabbea7b50b2eb0ae260cd33be84932c99f13c
James Mills <STEMfromMills@users.noreply.github.com>
2022-01-06 13:50:47 -0600

    Add files via upload

---
+++ a114_zero_iteration_and_infinite.py
@@ -0,0 +1,16 @@
+#   a114_zero_iteration_and_infinite.py
+#   Make a zero-iteration condition and follow it with an infinite loop.
+#   Include some visual evidence that the second loop is infinite.
+import turtle as trtl
+
+painter = trtl.Turtle()
+painter.speed(0)
+
+# Add a loop with a zero-iteration condition
+
+
+# Add an infinite loop
+
+
+wn = trtl.Screen()
+wn.mainloop()
