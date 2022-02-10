21d1fdc0158350db9344f6299c5a0fee40411861
stemfrommills <jmills@nevadacubs.org>
2022-02-10 13:39:03 -0600

    update

--- 2.2.7/p227_starter_one_button_shell.py
+++ 2.2.7/p227_starter_one_button_shell.py
@@ -5,15 +5,58 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command():
-    subprocess.call("ping localhost")
+# Modify the do_command function:
+#   to use the new button as needed
+def do_command(command):
+    global command_textbox, url_entry
+
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+    
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+    # use url_val 
+    p = subprocess.Popen(command + ' ::1', stdout=subprocess.PIPE, stderr=subprocess.PIPE) #v2
+
+    cmd_results, cmd_errors = p.communicate()
+    command_textbox.insert(tk.END, cmd_results)
+    command_textbox.insert(tk.END, cmd_errors)
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
+# Makes the command button pass it's name to a function using lambda
+ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", command=lambda:do_command("ping -t"))
 ping_btn.pack()
+
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
+    compound="center",
+    font=("comic sans", 14),
+    bd=0, 
+    relief=tk.FLAT, 
+    cursor="heart",
+    fg="mediumpurple3",
+    bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="black") # change frame color
+frame.pack()
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
 
 root.mainloop()
