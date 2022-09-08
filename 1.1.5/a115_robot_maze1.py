0944f9c83b9ffaff478364b7a9ede80eee4cd595
jamesmckennamero <james.mckennamero@nevadacubs.org>
2022-09-08 13:09:41 -0500

    temp save

--- 1.1.5/a115_robot_maze1.py
+++ 1.1.5/a115_robot_maze1.py
@@ -21,7 +21,7 @@ def turn_left():
 #----- init screen
 wn = trtl.Screen()
 wn.setup(width=screen_w, height=screen_h)
-robot_image = "robot.gif"
+robot_image = "1.1.5/robot.gif"
 wn.addshape(robot_image)
 
 #----- init robot
@@ -37,18 +37,18 @@ robot.speed(2)
 robot.showturtle()
 
 #---- TODO: change maze here
-wn.bgpic("maze1.png") # other file names should be maze2.png, maze3.png
+wn.bgpic("1.1.5/maze1.png") # other file names should be maze2.png, maze3.png
 
 #---- TODO: begin robot movement here
 # move robot forward with move()
 # turn robot left with turn_left()
 # sample while loop:
-'''
+
 i = 0
 while (i < 3): # forward 3
   move()
   i = i + 1 
-'''
+
 
 #---- end robot movement 
 
