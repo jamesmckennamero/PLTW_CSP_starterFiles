f905c5290d3fb0ca66cc9cc42bcf978bcf175664
jamesmckennamero <james.mckennamero@nevadacubs.org>
2022-09-14 13:02:29 -0500

    spider moment

--- 1.1.6/a116_buggy_image.py
+++ 1.1.6/a116_buggy_image.py
@@ -1,20 +1,51 @@
 #   a116_buggy_image.py
 import turtle as trtl
-# instead of a descriptive name of the turtle such as painter,
-# a less useful variable name x is used
-x = trtl.Turtle()
-x.pensize(40)
-x.circle(20)
-w = 6
-y = 70
-z = 380 / w
-x.pensize(5)
-n = 0
-while (n < w):
-  x.goto(0,0)
-  x.setheading(z*n)
-  x.forward(y)
-  n = n + 1
-x.hideturtle()
+# instead of a descriptive name of the turtle such as spider,
+# a less useful variable name spider is used
+spider = trtl.Turtle()
+
+#draw spider body
+spider.pensize(40)
+spider.circle(20)
+
+
+#spider leg configuration
+line = 4
+length = 90
+#length = 70
+angle = 120 / line
+spider.pensize(5)
+counter = 0
+
+#draw spider legs loop
+while (counter < line):
+  spider.goto(0,20)
+  spider.setheading(angle*counter -45)
+  spider.forward(length)
+  counter = counter + 1
+
+counter = 0
+
+while (counter < line):
+  spider.goto(0,20)
+  spider.setheading(angle*counter - 240)
+  spider.forward(length)
+  counter = counter + 1
+
+spider.penup()
+spider.goto(-15,55)
+spider.pendown()
+spider.pencolor("red")
+spider.pensize(10)
+spider.circle(5)
+
+spider.penup()
+spider.goto(15,55)
+spider.pendown()
+spider.pencolor("red")
+spider.pensize(10)
+spider.circle(5)
+
+spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
