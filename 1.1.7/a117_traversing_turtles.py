d5522ea3d328cfdf12613085e5d5d71aa125c959
jamesmckennamero <james.mckennamero@nevadacubs.org>
2022-09-20 13:12:03 -0500

    wip

--- 1.1.7/a117_traversing_turtles.py
+++ 1.1.7/a117_traversing_turtles.py
@@ -11,21 +11,28 @@ turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  new_color=turtle_colors.pop()
+  t.color(new_color)
   my_turtles.append(t)
 
-#  
+# sets turtle to origin point
 startx = 0
 starty = 0
 
-#
+heading = 90
+
+# directs where the turtle must go
 for t in my_turtles:
   t.goto(startx, starty)
-  t.right(45)     
-  t.forward(50)
 
-#	
-  startx = startx + 50
-  starty = starty + 50
+  t.right(heading) 
+  t.forward(50)
+  
+
+#	modifies starting point
+  startx = t.xcor()
+  starty = t.ycor()
+  heading=(heading+45)
 
 wn = trtl.Screen()
 wn.mainloop()
