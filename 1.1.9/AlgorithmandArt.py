13a2c2b571e98339d211279f8cfff4c5ce7426d6
jamesmckennamero <james.mckennamero@nevadacubs.org>
2022-09-29 13:12:14 -0500

    programming

--- 1.1.9/AlgorithmandArt.py
+++ 1.1.9/AlgorithmandArt.py
@@ -1,15 +1,18 @@
 #imports turtle
 import turtle as trtl
+import time as ti
 
 #creates lists
-turtle_shapes_main = ["arrow", "turtle", "triangle", "classic"]
-colors_main = ["red", "green", "orange", "brown"]
-turtle_shapes = []
+colors_main = ["red", "green", "brown", "gray"]
 colors = []
 
 painter = trtl.Turtle()
+
 painter.speed(100)
 
+print(colors)
+
+'''
   #redraws car shape
 painter.penup()
 painter.goto(280,-40)
@@ -45,11 +48,13 @@ painter.forward(150)
 painter.right(90)
 painter.forward(150)
 painter.right(90)
+'''
 
 
+'''
 #draws ground
 painter.penup()
-painter.goto(-625,-350)
+painter.goto(-500,-350)
 painter.right(90)
 painter.pendown()
 painter.circle(150,-180)
@@ -63,15 +68,52 @@ painter.left(180)
 painter.circle(150,-180)
 painter.left(180)
 
+#ground moving
+painter.penup()
+painter.goto(-550,-350)
+painter.pendown()
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+
+painter.penup()
+painter.goto(-600,-350)
+painter.pendown()
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+painter.circle(150,-180)
+painter.left(180)
+''''''
+
+
+
 #defines amount for time and frame
 time = 10
 frames = 3
-
 '''
 #loop for animation
 for t in range(time): 
     frames = 3
+    base_ground_location = -500,-350
     while frames > (0):
+        colors.append(colors_main)
+        maincolor = colors.pop()
+
         #redraws car shape
         painter.penup()
         painter.goto(300,-40)
@@ -81,12 +123,59 @@ for t in range(time):
         painter.circle(300,180)
         painter.left(90)
         painter.forward(600)
+
+        colors.pop()
+        #draws ground
+        painter.goto(-500,-350)
+        painter.penup()
+        painter.right(90)
+        painter.pendown()
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+
+        #ground moving
+        painter.penup()
+        painter.goto(-550,-350)
+        painter.pendown()
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+
+        painter.penup()
+        painter.goto(-600,-350)
+        painter.pendown()
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        painter.circle(150,-180)
+        painter.left(180)
+        
         
         frames = frames - (1)
 
         trtl.clearscreen()
-'''
-
 
 
 
