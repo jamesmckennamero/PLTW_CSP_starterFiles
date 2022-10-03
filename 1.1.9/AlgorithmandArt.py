d8273e7dff66574b2ee4eab2ba100227ca2d8f66
jamesmckennamero <james.mckennamero@nevadacubs.org>
2022-10-03 13:28:31 -0500

    somewhat working

--- 1.1.9/AlgorithmandArt.py
+++ 1.1.9/AlgorithmandArt.py
@@ -1,16 +1,13 @@
 #imports turtle
 import turtle as trtl
+import time as ti
 painter = trtl.Turtle()
 #creates lists
 
-red = "red"
-green = "green"
-black = "black"
-colors_main = "red", "green", "black"
-colors = []
+colors_main = ["red", "green", "black"]
 
 
-print(colors)
+
 
 painter.speed(100)
 
@@ -46,20 +43,14 @@ painter.right(90)
 
 #defines amount for time and frame
 time = 10
-frames = 3
 
 #loop for animation
 for t in range(time): 
-    frames = 3
-    while frames > (0):
-        colors.append(red)
-        colors.append(green)
-        colors.append(black)
-        currentcolor = colors
+    for colors in colors_main:
         painter.reset()
+        painter.pencolor(colors)
         painter.speed(100)
         #redraws car shape
-        painter.pencolor((currentcolor))
         painter.penup()
         painter.goto(300,-40)
         painter.pendown()
@@ -70,10 +61,35 @@ for t in range(time):
         painter.forward(600)
         painter.penup()
 
-        colors.pop()
-        #draws ground
-        painter.goto(-500,-350)
+        #draws left square
         painter.penup()
+        painter.goto(-250,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
+        #draws right square
+        painter.penup()
+        painter.goto(50,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
+        #draws ground
+        painter.penup()
+        painter.goto(-500,-350)
         painter.right(90)
         painter.pendown()
         painter.circle(150,-180)
@@ -87,9 +103,50 @@ for t in range(time):
         painter.circle(150,-180)
         painter.left(180)
 
+        ti.sleep(0.2)
+        trtl.clearscreen()
+        #second iteration
+        painter.penup()
+        painter.goto(300,-40)
+        painter.pendown()
+        painter.pensize(15)
+        painter.left(180)
+        painter.circle(300,180)
+        painter.left(90)
+        painter.forward(600)
+        painter.penup()
+
+        #draws left square
+        painter.penup()
+        painter.goto(-250,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
+        #draws right square
+        painter.penup()
+        painter.goto(50,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
         #ground moving
+
         painter.penup()
         painter.goto(-550,-350)
+        painter.right(90)
         painter.pendown()
         painter.circle(150,-180)
         painter.left(180)
@@ -102,8 +159,50 @@ for t in range(time):
         painter.circle(150,-180)
         painter.left(180)
 
+        ti.sleep(0.2)
+        painter.reset()
+        trtl.clearscreen()
+        painter.speed(100)
+        #third iteration
+        painter.penup()
+        painter.goto(300,-40)
+        painter.pendown()
+        painter.pensize(15)
+        painter.left(90)
+        painter.circle(300,180)
+        painter.left(90)
+        painter.forward(600)
+        painter.penup()
+
+        #draws left square
+        painter.penup()
+        painter.goto(-250,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
+        #draws right square
+        painter.penup()
+        painter.goto(50,-50)
+        painter.pendown()
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+        painter.forward(150)
+        painter.right(90)
+
         painter.penup()
         painter.goto(-600,-350)
+        painter.right(90)
         painter.pendown()
         painter.circle(150,-180)
         painter.left(180)
@@ -119,7 +218,6 @@ for t in range(time):
         painter.left(180)
         
         trtl.clearscreen()
-        frames = frames - (1)
 
         
 
